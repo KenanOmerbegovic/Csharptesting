cf09cb20db6e803995ff124dd16d19cf0735ed28
kenan <kenanomerbegovic@gmail.com>
2024-10-01 23:57:45 +0200

    first test

---
+++ Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClickThis = new System.Windows.Forms.Button();
+            this.lblHello = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnClickThis
+            // 
+            this.btnClickThis.Location = new System.Drawing.Point(338, 276);
+            this.btnClickThis.Name = "btnClickThis";
+            this.btnClickThis.Size = new System.Drawing.Size(91, 34);
+            this.btnClickThis.TabIndex = 0;
+            this.btnClickThis.Text = "Say Hello";
+            this.btnClickThis.UseVisualStyleBackColor = true;
+            this.btnClickThis.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblHello
+            // 
+            this.lblHello.AutoSize = true;
+            this.lblHello.Location = new System.Drawing.Point(350, 91);
+            this.lblHello.Name = "lblHello";
+            this.lblHello.Size = new System.Drawing.Size(65, 20);
+            this.lblHello.TabIndex = 1;
+            this.lblHello.Text = "Hi there";
+            this.lblHello.Click += new System.EventHandler(this.lblHello_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblHello);
+            this.Controls.Add(this.btnClickThis);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClickThis;
+        private System.Windows.Forms.Label lblHello;
+    }
+}
+
